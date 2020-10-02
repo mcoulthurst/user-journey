47a6c83c0a216a2a7d01d3b75fb9803d60de868b
mcoulthurst <mc@lightness.co.uk>
2020-10-02 15:52:18 +0100

    Display text blocks

--- src/index.js
+++ src/index.js
@@ -1,27 +1,110 @@
 import * as d3Base from 'd3'
+//import * as dsv from 'd3-dsv' // d3 submodule (contains d3.csv, d3.json, etc)
+import { textwrap } from 'd3-textwrap';
 
-const d3 = Object.assign(d3Base, { })
+const d3 = Object.assign(d3Base);
+d3.textwrap = textwrap;
 const URL = "assets/data/user.csv";
 const margin = { top: 40, right: 80, bottom: 0, left: 0 };
 var width = 1000;
 var height = 700;
 var svg;
 
-function processData(data) {
-  console.log(data);
+var ht_row = 170;
+const wd_row = 30;
+var wd_full = 1920;
+const wd_offset = 330;
+const ht_offset = 170;
 
+const ht = 150;
+const wd = 160;
+const gutter = 10;
+const gutterX = 5;
+const gutterY = 32;
+
+var row_x;
+var row_y;
+
+var offsetX = 2 * wd + gutterX;
+var offsetY = 110;
+
+function processData(arr) {
+  console.log(arr);
+ 
   if(arr.length<6 ){
       showError('Not enough Data');
   }else if(arr.length>20 ){
       showError('Too Many Rows');
   }else{
-      drawSidePanel(arr.slice(0,5), selection);
+      //drawSidePanel(arr.slice(0,5));
 
-      drawJourney(arr.slice(5), selection);
+      drawJourney(arr.slice(5));
   }
+
 }
 
-function draw() {
+function drawJourney(arr) {
+  const box = {width:120};
+  var wrap;
+// create a text wrapping function
+wrap = d3.textwrap()
+    // wrap to 480 x 960 pixels
+    .bounds({height: 100, width: 100})
+    // wrap with tspans in all browsers
+    .method('tspans');
+
+  const g = svg.selectAll('.title')
+  .data(arr)
+  .enter()
+  .append('g')
+  .attr('transform', `translate(${0},${50})`);
+
+  g.append('text')
+  .attr('transform', (d, i) => `translate(${i * box.width + 50}, -${20})`)
+  .attr('class', 'title')
+  .attr('fill', '#333')
+  .attr('stroke', '#333')
+  .attr('font-size', 14)
+  .attr('width', box.width)
+  .attr('height', box.width)
+  //.attr('font-family', 'Montserrat')
+  .attr('font-weight',100)
+  .text((d) => d.Tasks)
+  .attr('text-anchor', 'start');
+
+  g.append('text')
+  .attr('transform', (d, i) => `translate(${i * box.width + 50}, ${150})`)
+  .attr('class', 'title')
+  .attr('fill', '#333')
+  .attr('stroke', '#333')
+  .attr('font-size', 14)
+  .attr('width', box.width)
+  .attr('height', box.width)
+ // .attr('font-family', 'Montserrat')
+  .attr('font-weight',100)
+  .text((d) => d['Touch points'])
+  .attr('text-anchor', 'start');
+
+  g.append('text')
+  .attr('transform', (d, i) => `translate(${i * box.width + 50}, ${250})`)
+  .attr('class', 'title')
+  .attr('fill', '#333')
+  .attr('stroke', '#333')
+  .attr('font-size', 14)
+  .attr('width', box.width)
+  .attr('height', box.width)
+  //.attr('font-family', 'Montserrat')
+  .attr('font-weight',100)
+  .text((d) => d['Pain points'])
+  .attr('text-anchor', 'start');
+
+  var text = d3.selectAll('text');
+  // run the text wrapping function on all text nodes
+  text.call(wrap);
+
+}
+
+function initSVG() {
   svg = d3
     .select("#chart")
     .append("svg")
@@ -30,34 +113,20 @@ function draw() {
     .attr("margin-left", margin.left + "px")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 }
+function callback(data){
+  console.log(data);
 
-
+}
 document.addEventListener("DOMContentLoaded", function () {
   console.log("ready");
-  // load data
-  d3.csv(URL, function(data){
-    console.log(data)
-});
 
-/* 
-  d3.csv(URL, function (data) {
-    
+  d3.dsv(",", URL, function(d) {
+    return d;
+  }).then(function(data) {
     console.log(data);
-    
-    var dataX = d3.csvParseRows(data, function(d, i) {
-      console.log(d);
-      return {
-         year: new Date(+d[0], 0, 1), // convert first colum column to Date
-        make: d[1],
-        model: d[2],
-        length: +d[3] // convert fourth column to number 
-      };
-    });
-    console.log(dataX);
-
     processData(data);
-    draw();
-  }); 
+  });
 
- */
+  initSVG();
+
 });
