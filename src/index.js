2109ca528e98263f1f74a6360c646c0f22ce1ff5
mcoulthurst <mc@lightness.co.uk>
2020-10-02 18:25:19 +0100

    Add bezier curve

--- src/index.js
+++ src/index.js
@@ -39,7 +39,75 @@ function processData(arr) {
       //drawSidePanel(arr.slice(0,5));
 
       drawJourney(arr.slice(5));
+      drawCurves(arr.slice(5));
   }
+
+}
+
+function gradient(a, b) { 
+  return (b.y-a.y)/(b.x-a.x); 
+} 
+
+function bzCurve(points, f, t) { 
+  if (typeof(f) == 'undefined') f = 0.3; 
+  if (typeof(t) == 'undefined') t = 0.6; 
+
+  ctx.beginPath(); 
+  ctx.moveTo(points[0].x, points[0].y); 
+
+  var m = 0; 
+  var dx1 = 0; 
+  var dy1 = 0; 
+
+  var preP = points[0]; 
+
+  for (var i = 1; i < points.length; i++) { 
+      var curP = points[i]; 
+      nexP = points[i + 1]; 
+      if (nexP) { 
+          m = gradient(preP, nexP); 
+          dx2 = (nexP.x - curP.x) * -f; 
+          dy2 = dx2 * m * t; 
+      } else { 
+          dx2 = 0; 
+          dy2 = 0; 
+      } 
+        
+      ctx.bezierCurveTo( 
+          preP.x - dx1, preP.y - dy1, 
+          curP.x + dx2, curP.y + dy2, 
+          curP.x, curP.y 
+      ); 
+    
+      dx1 = dx2; 
+      dy1 = dy2; 
+      preP = curP; 
+  } 
+  ctx.stroke(); 
+} 
+
+function drawCurves(arr){
+  var lines = [];   
+   for (var i = 0; i < arr.length; i++ ) { 
+    //translate(${i * box.width + 100 }, ${350  + 320 - (d.Emotion*30)}) rotate(45 5 5) `)
+      var yPos = 350  + 320 - (arr[i].Emotion*30); 
+      var xPos = i * 120 + 100;
+     // var pt = { x: xPos, y: yPos }; 
+      var pt = [xPos, yPos]; 
+      lines.push(pt); 
+  } 
+  console.log(lines);
+
+  const line2 = d3.line().curve(d3.curveCardinal);
+  console.log(line2(lines));
+
+  var path = svg.append('path')
+    .attr('d', line2(lines))
+    .attr("stroke", 'black')
+    .attr("stroke-width", 1)
+    .attr("fill", "none");
+
+    console.log('Append path', path);
 
 }
 
@@ -60,7 +128,7 @@ wrap = d3.textwrap()
   //.attr('transform', `translate(${0},${50})`);
 
   g.append('text')
-  .attr('transform', (d, i) => `translate(${i * box.width + 50}, -${20})`)
+  .attr('transform', (d, i) => `translate(${i * box.width + 50}, ${20})`)
   .attr('class', 'title')
   .attr('fill', '#333')
   .attr('stroke', '#333')
@@ -73,7 +141,7 @@ wrap = d3.textwrap()
   .attr('text-anchor', 'start');
 
   g.append('text')
-  .attr('transform', (d, i) => `translate(${i * box.width + 50}, ${150})`)
+  .attr('transform', (d, i) => `translate(${i * box.width + 50}, ${200})`)
   .attr('class', 'title')
   .attr('fill', '#333')
   .attr('stroke', '#333')
@@ -104,20 +172,20 @@ wrap = d3.textwrap()
   .attr('stroke', '#999')
   .attr('stroke-opacity', 1)
   .attr('stroke-width', 1)
+  .attr('x', -5)
+  .attr('y', -5)
   .attr('width', 10)
   .attr('height', 10)
   .attr('rotation', 90)
-  .attr('transform', (d, i) => `translate(${i * box.width + 100 }, ${350  + 320 - (d.Emotion*30)}) rotate(45 5 5) `)
-
-
+  .attr('transform', (d, i) => `translate(${i * box.width + 100 }, ${350  + 320 - (d.Emotion*30)}) rotate(45 0 0) `)
 
   var text = d3.selectAll('text');
   // run the text wrapping function on all text nodes
   text.call(wrap);
 
-
-
 }
+
+
 
 function initSVG() {
   svg = d3
